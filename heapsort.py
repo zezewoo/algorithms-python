c2606c8f1a524ab09963d7b4109a80f3bb051dff
zezewoo <zezewoo@163.com>
2016-03-21 14:00:26 +0800

    add heapsort

---
+++ heapsort.py
@@ -0,0 +1,29 @@
+
+def left(i):
+	return i<<1
+
+def right(i):
+	return i<<1+1
+
+def parent(i):
+	return i>>1
+
+
+def max_heapify(A, i):
+	l = left(i)
+	r = right(i)
+	lagest = 0
+	if l <= len(A) and A[l] > A[i]:
+		lagest = l
+	else:
+		lagest = i
+	
+	if r <= len(A) and A[r] > A[lagest]:
+		lagest = r
+	if lagest != i:
+		tmp = A[i]
+		A[i] = A[lagest]
+		A[lagest] = tmp
+		max_heapify(A, lagest)
+	
+	
