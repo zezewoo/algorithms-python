f4585f796ec3ceecd4676dffca4ce9337efd7e07
zezewoo <zezewoo@163.com>
2016-11-03 14:27:56 +0800

    new changes

--- graph/graph.py
+++ graph/graph.py
@@ -28,3 +28,31 @@ G = some_graph()
 for i in range(len(G)):
 	G[i] = set(G[i])
 print list(walk(G, 0))
+
+def tr(G):
+	GT = {}
+	for u in G: GT[u] = set()
+	for u in G:
+		for v in G[u]:
+			GT[v].add(u)
+	return GT
+
+def scc(G):
+	GT = tr(G)
+	sccs, seen = [], set()
+	for u in dfs_topsort(G):
+		if u in seen: continue
+		C = walk(GT, u, seen)
+		seen.update(C)
+		sccs.append(C)
+	return sccs
+
+from string import ascii_lowercase
+def parse_graph(s):
+	G = {}
+	for u, line in zip(ascii_lowercase, s.split("/")):
+		G[u] = set(line)
+	return G
+
+G = parse_graph('bc/die/d/ah/f/g/eh/i/h')
+print list(map(list, scc(G)))
