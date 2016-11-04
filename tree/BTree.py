c92628f56c2b2c3a03cbec937afdeb46a8d4fa2d
zezewoo <zezewoo@163.com>
2016-11-04 17:33:53 +0800

    tree

--- tree/BTree.py
+++ tree/BTree.py
@@ -15,24 +15,41 @@ class BTree:
 		print 'key is %d' % key
 		print 'name is ', name
 		node = nd.Node(key, name, None, None)	
-		
+		if self.root == None:
+			self.root = node
+			return
 
+		current = self.root
+		while current:
+			print 'trace ..'
+			if current.key > key and current.left != None:
+				print 'trace left'
+				current = current.left
+			elif current.key < key and current.right != None:
+				print 'trace right'
+				current = current.right
+			else :
+				break
+		current.left, node.left = node, current.left
+		
 	def displayPre(self, node):
 		print '!***display nodes of the tree***!'
 		print 'key is %d' % node.key
 		print 'name is ', node.name
 		if node.left:
-			self.display(node.left)
+			self.displayPre(node.left)
 		if node.right:
-			self.display(node.right)
+			self.displayPre(node.right)
 
 if __name__ == '__main__':
 	
 	l1_left = nd.Node(5, 'tom', None, None)
-	l2_right = nd.Node(8, 'lily', None, None)
-	root = nd.Node(1, 'henry', l1_left, l2_right)
+	l2_right = nd.Node(18, 'lily', None, None)
+	root = nd.Node(10, 'henry', l1_left, l2_right)
 	
 	bt = BTree(root)
-	bt.display(root)
+	# bt.displayPre(root)
 
+	bt.add(4, 'jack')
+	bt.displayPre(root)
 
